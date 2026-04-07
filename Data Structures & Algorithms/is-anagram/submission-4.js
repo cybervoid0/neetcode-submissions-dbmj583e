c04ea698856df0ad585a3dc155910ba3f97ac124
Cybervoid <shenshin@me.com>
2026-04-07 19:49:38 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        // const freqS = this.#freq(s)
+        // const freqT = this.#freq(t)
+        // const arrS = Object.entries(freqS)
+        // const arrT = Object.entries(freqT)
+        // return arrS.length == arrT.length && arrS.every(([key, val]) => freqT[key] == val)
+        return String(Array.from(s).toSorted()) === String(Array.from(t).toSorted())
+    }
+
+    #freq(str) {
+        return Array.from(str).reduce((acc, el) => ({ ...acc, [el]: (acc[el] ?? 0) + 1 }), {})
+    } 
+}
