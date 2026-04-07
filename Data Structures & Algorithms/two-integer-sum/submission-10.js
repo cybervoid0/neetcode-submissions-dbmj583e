e529721e1198878411e03245005f8771b611b833
Cybervoid <shenshin@me.com>
2026-04-07 23:18:31 +0200

    Add: two-integer-sum - submission-10

---
+++ Data Structures & Algorithms/two-integer-sum/submission-10.js
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        const seen = new Map()
+        for (let [i, num] of nums.entries()) {
+            let pair = target - num
+            if (seen.has(pair)) return [seen.get(pair), i]
+            seen.set(num, i)
+        }
+    }
+}
