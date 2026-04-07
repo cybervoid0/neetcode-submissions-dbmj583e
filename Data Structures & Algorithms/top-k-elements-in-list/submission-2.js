a6140c1157fdcc3fa97667e354506ecce5fb53c2
Cybervoid <shenshin@me.com>
2026-04-08 00:31:11 +0200

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums, k) {
+        const freq = new Map()
+        for (const num of nums) {
+            freq.set(num, (freq.get(num) ?? 0) + 1)
+        }
+        const buckets = freq.entries().reduce((acc, [num, count]) => {
+            acc[count] = [...(acc[count] ?? []), num]
+            return acc
+        }, [])
+        return buckets.flat().slice(-k)
+    }
+}
